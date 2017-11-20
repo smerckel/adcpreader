3918545d336b7c714a89f14eb07b13c4805cb79e
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2017-11-20 11:04:54 +0100

    Updated the list of importable modules.

--- rdi/__init__.py
+++ rdi/__init__.py
@@ -1,4 +1,4 @@
-__all__ = 'rdi_reader rdi_ndf rdi_transforms rdi_corrections'.split()
+__all__ = 'rdi_reader rdi_ndf rdi_transforms rdi_corrections rdi_qc rdi_writer rdi_backscatter'.split()
 
 
 __VERSION__ = '0.0.1'
