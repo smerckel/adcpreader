58159507a8eb6744eb2319e640d561b1ae6f9a48
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2019-01-18 14:55:30 +0100

    Added new operator VelocityRangeLimit, which masks part of the velocity profile that originates from with the sea bed.

--- rdi/rdi_qc.py
+++ rdi/rdi_qc.py
@@ -3,6 +3,7 @@ import numpy as np
 from rdi import __VERSION__
 
 from rdi.coroutine import coroutine, Coroutine
+from rdi.rdi_transforms import RotationMatrix
 
 # default setting for if true, all ensembles that have all data
 # blanked out because of some quality check will silently be dropped.
@@ -54,56 +55,60 @@ class QualityControl(Coroutine):
         ''' check an ensemble. Should be subclassed.'''
         raise NotImplementedError
 
-    def discard_greater(self, v, value):
+    def discard_greater(self, v, value, vp=None):
         ''' discard values v that are greater than value '''
         condition = v>value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_greater_equal(self, v, value):
+    def discard_greater_equal(self, v, value, vp=None):
         ''' discard values v that are greater or equal than value '''
         condition = v>=value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_less(self, v, value):
+    def discard_less(self, v, value, vp=None):
         ''' discard values v that are less than value '''
         condition = v<value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_less_equal(self, v, value):
+    def discard_less_equal(self, v, value, vp=None):
         ''' discard values v that are less or equal than value '''
         condition = v<value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_abs_greater(self, v, value):
+    def discard_abs_greater(self, v, value, vp=None):
         ''' discard values v that are absolute greater than value '''
         condition = np.abs(v)>value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_abs_greater_equal(self, v, value):
+    def discard_abs_greater_equal(self, v, value, vp=None):
         ''' discard values v that are absolute greater or equal than value '''
         condition = np.abs(v)>=value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_abs_less(self, v, value):
+    def discard_abs_less(self, v, value, vp=None):
         ''' discard values v that are absolute less than value '''
         condition = np.abs(v)<value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def discard_abs_less_equal(self, v, value):
+    def discard_abs_less_equal(self, v, value, vp=None):
         ''' discard values v that are absolute less or equal than value '''
         condition = np.abs(v)<value
-        return self.apply_condition(v, condition)
+        return self.apply_condition(v, condition, vp)
 
-    def apply_condition(self, v, condition):
+    def apply_condition(self, v, condition, vp=None):
+        if not vp is None:
+            _v = vp
+        else:
+            _v = v
         try:
-            v.mask |= condition 
+            _v.mask |= condition 
         except AttributeError as e:
             if e.args[0] == "'numpy.ndarray' object has no attribute 'mask'":
-                v = np.ma.masked_array(v, condition)
+                _v = np.ma.masked_array(_v, condition)
             else:
                 if condition:
-                    v = np.nan
-        return v
+                    _v = np.nan
+        return _v
     
                 
 class ValueLimit(QualityControl):
@@ -223,9 +228,222 @@ class AcousticAmplitudeLimit(QualityControl):
             ens['velocity'][s] = self.apply_condition(ens['velocity'][s], condition)
         return True # always return the ensemble
 
+class KalmanFilter(object):
+    ''' A Kalman filter implementation to estimate the water depth.
 
-
+    The measurements are: depth of DVL/ADCP
+                          range till bottom
+    '''
     
+    def __init__(self, qw, qH, rz, rH, pitch_min = 5, pitch_max = 45):
+        '''
+        Parameters
+        ----------
+        qw: float
+            uncertainty for velocity model
+        qH: float
+            uncertainty for water depth model
+        rz: float
+            uncertainty in depth reading
+        rH: float
+            uncertainty in range reading
+        pitch_min: float
+            min value of (absolute) pitch when depth readings are considered
+            unit: degree
+        pitch_max: float
+            max value of (absolute) pitch for which depth readings are considered
+            unit: degree
+        '''
+        self.noise_settings = dict(qw=qw, qH=qH, rz=rz, rH=rH)
+        self.t0 = None
+        self.F = np.array([[1., 0., 0.],
+                           [0., 1., 0.],
+                           [0., 0., 1.]])
+        self.Hzh = np.array([[1., 0., 0.],
+                             [0., 0., 1.]])
+        self.pitch_min_rad = pitch_min * np.pi/180.
+        self.pitch_max_rad = pitch_max * np.pi/180
+        # if we measure the positino only, then R == r
+        self.I = np.eye(3)
+        
+    def initial_step(self, t, z):
+        r = self.noise_settings['rz']
+        self.x_post = np.array([[z, 0,100]]).T
+        self.P_post = np.diag([r, 1, 100])
+        
+    def update(self, t, z, pitch, h):
+        ''' Updates Kalman filter
+
+        Parameters
+        ----------
+        t: float
+           time in seconds
+        z: float
+           depth reading of ADCP/DVL
+        pitch: float
+            measured pitch
+        h: float
+           measured depth range (from ADCP to the sea bed)
+        
+        Returns
+        -------
+        x_post : array of floats
+              posterior estimate of the state vector (depth, U_glider, water_depth)
+        P_post : 3x3 array of floats
+              posterior covariance matrix
+        '''
+        if self.t0 is None:
+            self.initial_step(t, z)
+        else:
+            sn = np.sin(pitch)
+            pitch_abs = np.abs(pitch)
+            dt = t - self.t0
+            # set model uncertainties
+            qH =self.noise_settings['qH'] # always like this.
+            qw = self.noise_settings['qw']
+            q = np.array([[dt**2/2 * np.abs(sn)*qw, dt/2*qw, 2*qH]]).T
+            Q = np.diag(q)
+            if pitch_abs>self.pitch_min_rad and pitch_abs<self.pitch_max_rad and z>5:
+                # the measurement uncertainties
+                rz = self.noise_settings['rz'] # depth reading is always like this
+            else:
+                rz=1000
+            if h>1: # valid reading
+                rh = self.noise_settings['rH']
+            else:
+                rh=1000 # if h==0 we didn't get a reading. Set its uncertainty to very high
+            H = self.Hzh
+            # the measurement:
+            y = np.array([[z, z+h]]).T
+            R = np.diag([rz, rh])
+            
+            I = self.I
+            P_post = self.P_post
+            x_post = self.x_post
+            #
+            F = self.F
+            F[0,1] = dt *sn
+            P_pre = F @ P_post @ F.T + Q
+            K = P_pre @ H.T @ np.linalg.inv(H @ P_pre @ H.T + R)
+            x_pre = F @ x_post
+            self.x_post = x_pre + K @ ( y - H @ x_pre)
+            self.P_post = (I -K @ H) @ P_pre @ (I - K @ H).T + K @ R @ K.T
+        self.t0 = t
+        return self.x_post, self.P_post
+
+
+
+class VelocityRangeLimit(QualityControl):
+    ''' Quality check for measured velocity profiles.
+
+    Any velocity readings appear to originate from the sea bottom and deeper are
+    masked.
+    Additionally, an estimate of the water depth is added to the ensemble.
+    '''
+    
+    def __init__(self, pitch_mount_angle, XdcrDepth_scale_factor = 10,qw=0.005**2, qH=0.001**2, rz = 0.15**2, rH=0.20**2):
+        '''
+        Parameters
+        ----------
+        pitch_mount_angle: float
+            mounting angle of the DVL in degrees
+        XdcrDepth_scale_factor: float (default: 10)
+            a scale factor to convert depth to meters 
+        
+        qw: float (default 0.005**2)
+        qH: float (default 0.001**2)
+        rz: float (default 0.15**2)
+        rH: flaot (default 0.20**2)
+            KalmanFilter settings
+        '''
+        super().__init__()
+        self.XdcrDepth_scale_factor = XdcrDepth_scale_factor
+        self.T = RotationMatrix()
+        self.nbeams = None
+        self.theta = None
+        self.kf = KalmanFilter(qw=qw, qH =qH, rz=rz, rH=rH)
+        self.pitch_mount_angle = pitch_mount_angle*np.pi/180.
+        
+    def get_fixed_leader_data(self, ens):
+        if ens['fixed_leader']['Xdcr_Facing']!='Down':
+            raise ValueError('The VelocityRangeLimit operator requires the DVL/ADCP to be downward looking')
+            # this is not strictly necessary, but if uipward looking the B array is different, and possibly some
+            # other things that I have not thought about, as it is not relevant for glider work.
+        # gets data from the fixed leader and other one off computations
+        nbeams = ens['fixed_leader']['N_Beams']
+        theta_deg, unit = ens['fixed_leader']['Beam_Angle'].split()
+        theta = np.pi/180.*float(theta_deg)
+        self.nbeams = nbeams
+        self.theta = theta
+        self.B = np.array([[-np.sin(theta), np.sin(theta), 0. , 0. ],
+                           [0., 0., np.sin(theta), -np.sin(theta) ],
+                           [np.cos(theta), np.cos(theta), np.cos(theta), np.cos(theta)],
+                           [0 , 0, 0, 0]])
+        n_cells = ens['fixed_leader']['N_Cells']
+        bin_size = ens['fixed_leader']['DepthCellSize']
+        r0 = ens['fixed_leader']['FirstBin']
+        self.ri = np.arange(n_cells)  * bin_size + r0
+        
+    def check_ensemble(self, ens):
+        '''check ensemble for passing/nonpassing of velocity profile data
+        
+        Parameters
+        ----------
+        ens : dictionary
+            ensemble dictionary
+
+        Returns
+        -------
+        True  
+            
+
+        The strategy followed here in is the following. The DVL
+        records the distance to the seabed as seen by each of its four
+        beams. Sometimes velotity readings are registered that seem to
+        come from within the seabed due to reflections I suppose. We
+        compute the distance from the transducer for the beam that has
+        observed the smallest range, as this one, and all the others
+        will contaminate the velocity reading. This cell and all
+        further cells are masked. 
+        
+            It may be that when this applies the current ensemble has
+        no informtion on bottom range. To fill in these data, a Kalman
+        filter is used. I am not entirely sure whether this is really
+        necessary. The upshot is that we get an estiamte of the water
+        depth, which is written to the bottom_track section, as
+        variable 'WaterDepth'.
+        '''
+        if self.nbeams is None:
+            self.get_fixed_leader_data(ens)
+
+        ranges = np.array([ens['bottom_track']['Range%d'%(i+1)] for i in range(self.nbeams)])
+        z_dvl = ens['variable_leader']['XdcrDepth'] * self.XdcrDepth_scale_factor
+        pitch = ens['variable_leader']['Pitch']  * np.pi/180
+        roll = ens['variable_leader']['Roll']  * np.pi/180
+        T = self.T(0, pitch, roll)
+        Bp = T @ self.B
+        h = self.correct_depth_reading(ranges, pitch, roll)
+        hm = np.median(h, axis=0)
+        ens['bottom_track']['WaterDepth'] = hm + z_dvl
+        R = hm / Bp[2]
+        Rmin = np.min(R)
+        r = Rmin/np.cos(self.theta) #length of shortest beam
+        for i in range(self.nbeams):
+            s = 'Velocity%d'%(i+1)
+            ens['velocity'][s] = self.discard_greater(self.ri, r, ens['velocity'][s])
+        return True # always return the ensemble
+
+
+    def correct_depth_reading(self, h, pitch, roll):
+        # see http://psc.apl.uw.edu/HLD/Bstrait/WoodgateandHolroyd2011_BTrangeCorrection.pdf
+        sn_theta = np.sin(self.theta)
+        p = pitch + self.pitch_mount_angle
+        r = roll
+        term2 = np.cos(self.theta) * np.sqrt(1 - np.sin(r)**2 - np.sin(p)**2)
+        cosai = np.array([np.sin(r), -np.sin(r), -np.sin(p), +np.sin(p)])*sn_theta + term2
+        f = cosai/np.cos(self.theta) 
+        return  f * h
+
 class Counter(Coroutine):
     ''' An ensemble counter class.
 
--- tests/test_rdi.py
+++ tests/test_rdi.py
@@ -1,9 +1,12 @@
+import unittest
 import numpy as np
 
-import rdi.rdi_reader, rdi.rdi_writer, rdi.rdi_qc, rdi.rdi_transforms, rdi.rdi_corrections, rdi.rdi_backscatter
-
-
-import unittest
+if 1: # use local install
+    import sys
+    sys.path.insert(0, '../rdi')
+    import rdi_reader, rdi_writer, rdi_qc, rdi_transforms, rdi_corrections, rdi_backscatter
+else:
+    from rdi import rdi_reader, rdi_writer, rdi_qc, rdi_transforms, rdi_corrections, rdi_backscatter
 
 
 class TestWriter(unittest.TestCase):
@@ -14,46 +17,46 @@ class TestWriter(unittest.TestCase):
         pass
 
     def test_ascii(self):
-        pd0 = rdi.rdi_reader.PD0()
+        pd0 = rdi_reader.PD0()
         with open('PF230519.txt', 'w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             pd0.send_to(writer)
             pd0.process(self.filename)
 
     def test_ndf(self):
-        pd0 = rdi.rdi_reader.PD0()
-        writer = rdi.rdi_writer.NDFWriter('PF230519.ndf')
+        pd0 = rdi_reader.PD0()
+        writer = rdi_writer.NDFWriter('PF230519.ndf')
         pd0.send_to(writer)
         pd0.process(self.filename)
 
             
     def test_netcdf(self):
-        pd0 = rdi.rdi_reader.PD0()
-        writer = rdi.rdi_writer.NetCDFWriter('PF230519.nc')
+        pd0 = rdi_reader.PD0()
+        writer = rdi_writer.NetCDFWriter('PF230519.nc')
         with writer:
             pd0.send_to(writer)
             pd0.process(self.filename)
 
     def test_qc(self):
-        pd0 = rdi.rdi_reader.PD0()
+        pd0 = rdi_reader.PD0()
         
-        qc = rdi.rdi_qc.ValueLimit()
+        qc = rdi_qc.ValueLimit()
         qc.set_discard_condition('velocity', 'Velocity1', '||>', 1)
         with open('tmp','w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             pd0.send_to(qc)
             qc.send_to(writer)
             pd0.process(self.filename)
         
     def test_transforms_to_beams(self):
-        pd0 = rdi.rdi_reader.PD0()
+        pd0 = rdi_reader.PD0()
 
-        t1 = rdi.rdi_transforms.TransformENU_SFU()
-        t2 = rdi.rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
-        t3 = rdi.rdi_transforms.TransformXYZ_BEAM()
+        t1 = rdi_transforms.TransformENU_SFU()
+        t2 = rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
+        t3 = rdi_transforms.TransformXYZ_BEAM()
         t = t3*t2*t1
         with open('tmp','w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             
             pd0.send_to(t)
             t.send_to(writer)
@@ -61,34 +64,34 @@ class TestWriter(unittest.TestCase):
             pd0.process(self.filename)
 
     def test_transforms(self):
-        pd0 = rdi.rdi_reader.PD0()
+        pd0 = rdi_reader.PD0()
 
-        t = rdi.rdi_transforms.TransformENU_SFU()
+        t = rdi_transforms.TransformENU_SFU()
 
         with open('tmp5','w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             pd0.send_to(t)
             t.send_to(writer)
             pd0.process(self.filename)
 
     def test_salinity_correction(self):
-        pd0 = rdi.rdi_reader.PD0()
-        t1 = rdi.rdi_transforms.TransformENU_SFU()
-        t2 = rdi.rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
-        t3 = rdi.rdi_transforms.TransformXYZ_BEAM()
+        pd0 = rdi_reader.PD0()
+        t1 = rdi_transforms.TransformENU_SFU()
+        t2 = rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
+        t3 = rdi_transforms.TransformXYZ_BEAM()
         t = t3*t2*t1
 
-        T1 = rdi.rdi_transforms.TransformSFU_ENU()
-        T2 = rdi.rdi_transforms.TransformXYZ_SFU(0, 0.1919, 0)
-        T3 = rdi.rdi_transforms.TransformBEAM_XYZ()
+        T1 = rdi_transforms.TransformSFU_ENU()
+        T2 = rdi_transforms.TransformXYZ_SFU(0, 0.1919, 0)
+        T3 = rdi_transforms.TransformBEAM_XYZ()
         T = T1*T2*T3
 
-        sosc = rdi.rdi_corrections.CurrentCorrectionFromSalinity(SA=7)
+        sosc = rdi_corrections.CurrentCorrectionFromSalinity(SA=7)
         
         with open('tmp1','w') as fp:
             with open('tmp2','w') as fp2:
-                writer = rdi.rdi_writer.AsciiWriter(fp)
-                writer2 = rdi.rdi_writer.AsciiWriter(fp2)
+                writer = rdi_writer.AsciiWriter(fp)
+                writer2 = rdi_writer.AsciiWriter(fp2)
                 # branch to writer and transform:
                 pd0.send_to(writer)
                 pd0.send_to(t)
@@ -100,28 +103,28 @@ class TestWriter(unittest.TestCase):
                 pd0.process(self.filename)
             
     def test_aggregator(self):
-        pd0 = rdi.rdi_reader.PD0()
-        agg = rdi.rdi_corrections.Aggregator(60)
+        pd0 = rdi_reader.PD0()
+        agg = rdi_corrections.Aggregator(60)
         with open('tmp3', 'w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             pd0.send_to(agg)
             agg.send_to(writer)
             pd0.process(self.filename)
 
     def test_attitude_correction(self):
-        pd0 = rdi.rdi_reader.PD0()
-        att = rdi.rdi_corrections.AttitudeCorrectionTiltCorrection(0.83, 0, 0, 'rotation')
+        pd0 = rdi_reader.PD0()
+        att = rdi_corrections.AttitudeCorrectionTiltCorrection(0.83, 0, 0, 'rotation')
         with open('tmp4', 'w') as fp:
-            writer = rdi.rdi_writer.AsciiWriter(fp)
+            writer = rdi_writer.AsciiWriter(fp)
             pd0.send_to(att)
             att.send_to(writer)
             pd0.process(self.filename)
 
     def test_backscatter(self):
-        pd0 = rdi.rdi_reader.PD0()
-        att = rdi.rdi_corrections.AttitudeCorrectionTiltCorrection(0.83, 0, 0, 'rotation')
-        backscatter =rdi.rdi_backscatter.AcousticCrossSection(S=7, k_t=1e-8, N_t=45, db_per_count=[0.61]*4)
-        writer = rdi.rdi_writer.NDFWriter('backscatter.ndf')
+        pd0 = rdi_reader.PD0()
+        att = rdi_corrections.AttitudeCorrectionTiltCorrection(0.83, 0, 0, 'rotation')
+        backscatter =rdi_backscatter.AcousticCrossSection(S=7, k_t=1e-8, N_t=45, db_per_count=[0.61]*4)
+        writer = rdi_writer.NDFWriter('backscatter.ndf')
 
         pd0.send_to(att)
         att.send_to(backscatter)
@@ -130,26 +133,47 @@ class TestWriter(unittest.TestCase):
         pd0.process(self.filename)
 
     def test_pipeline(self):
-        t1 = rdi.rdi_transforms.TransformENU_SFU()
-        t2 = rdi.rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
-        t3 = rdi.rdi_transforms.TransformXYZ_BEAM()
+        t1 = rdi_transforms.TransformENU_SFU()
+        t2 = rdi_transforms.TransformSFU_XYZ(0, 0.1919, 0)
+        t3 = rdi_transforms.TransformXYZ_BEAM()
         t = t3*t2*t1
 
-        T1 = rdi.rdi_transforms.TransformSFU_ENU()
-        T2 = rdi.rdi_transforms.TransformXYZ_SFU(0, 0.1919, 0)
-        T3 = rdi.rdi_transforms.TransformBEAM_XYZ()
+        T1 = rdi_transforms.TransformSFU_ENU()
+        T2 = rdi_transforms.TransformXYZ_SFU(0, 0.1919, 0)
+        T3 = rdi_transforms.TransformBEAM_XYZ()
         T = T1*T2*T3
 
-        sosc = rdi.rdi_corrections.CurrentCorrectionFromSalinity(SA=7)
-        writer = rdi.rdi_writer.DataStructure()
+        sosc = rdi_corrections.CurrentCorrectionFromSalinity(SA=7)
+        writer = rdi_writer.DataStructure()
 
-        pd0 = rdi.rdi_reader.PD0()
-        pipeline = rdi.rdi_reader.make_pipeline(t, sosc, T,writer)
+        pd0 = rdi_reader.PD0()
+        pipeline = rdi_reader.make_pipeline(t, sosc, T,writer)
         pd0.send_to(pipeline)
         pd0.process(self.filename)
         print(np.mean(pipeline.data['velocity_east']))
         #should be -6.199 or something
+
+    def test_Rangelimit(self):
+        pd0 = rdi_reader.PD0()
+        att = rdi_corrections.AttitudeCorrectionTiltCorrection(0.83, 0, 0, 'rotation')
+        rl = rdi_qc.VelocityRangeLimit(pitch_mount_angle=11,
+                                           XdcrDepth_scale_factor = 10,
+                                           qw=0.005**2, qH=0.001**2, rz = 0.15**2, rH=0.20**2)
+        writer = rdi_writer.NDFWriter('velocity_range_limit.ndf')
+        writer.set_custom_parameter('bottom_track', 'WaterDepth', dtype='scalar')
+        with writer:
+            pd0.send_to(att)
+            att.send_to(rl)
+            rl.send_to(writer)
+            pd0.process(self.filename)
+        
 if __name__=="__main__":
-    unittest.main()        
+    unittest.main()
+    if 0:
+        t = TestWriter()
+        t.setUp()
+        t.test_Rangelimit()
 
-
+        import ndf
+        data = ndf.NDF("velocity_range_limit.ndf", open_mode='open')
+        tm, dpt = data.get('variable_leader WaterDepth')
